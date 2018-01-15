1bdb093d0cc52a21764952b5160495698c42a945
alainrollejr <35453310+alainrollejr@users.noreply.github.com>
2018-01-15 13:22:52 +0100

    adding usecases

---
+++ usecases/check_powercontrol.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import hrclogutils.basic as hrc
+
+# replace path to appropriate location for your analysis
+df = hrc.load_rtce_log("../tests/sbc_rtce_monitor.csv","../tests/sbc_rtce_monitor.headers")
+
+
+# filter on a terminal, replace terminal id according to your needs 
+terminal_id = '15843'
+df = df.pipe(hrc.filter_name,terminal_id).pipe(hrc.filter_loggedon)
+
+print(df.head())
+
+print(df.info()) # gives idea what can be asked to plot
+
+df.pipe(hrc.plot_utc_sof,'Next.TxPSD','txPsdLimit')
+
+df.pipe(hrc.plot_utc_sof,'fbRequestedPower','fbAppliedPower','reportedPuSat')
