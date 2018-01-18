585acd30dbb4c51f4ee07f50f13d820ca548916d
alainrollejr <35453310+alainrollejr@users.noreply.github.com>
2018-01-18 09:51:02 +0100

    usecases can now be run with arguments that take path to log files

--- usecases/check_logon_stats.py
+++ usecases/check_logon_stats.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+ Visualises logon carousel
+ 
+ python check_logon_stats path_to_rtce_csv (optional: path to rtce headers file)
+ 
+ in spyder IDE:
+     %run check_logon_stats path_to_rtce_csv (optional: path to rtce headers file)
 """
 
 import os
@@ -12,20 +15,28 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import hrclogutils.basic as hrc
 import hrclogutils.rtce as rtce
 
-# replace path to appropriate location for your analysis
-df = rtce.load_rtce_log("./sbc_rtce_monitor.csv","../tests/sbc_rtce_monitor.headers")
 
 
-# filter to display terminals only when they are logging on
-df = df.pipe(rtce.filter_loggingon)
-df = df.reset_index()
+def main(argv):
+    if len(argv)==3:
+        df = rtce.load_rtce_log(argv[1],argv[2])
+    else:
+        df = rtce.load_rtce_log(argv[1], "../tests/sbc_rtce_monitor.headers")
+    
+    
+    # filter to display terminals only when they are logging on
+    df = df.pipe(rtce.filter_loggingon)
+    df = df.reset_index()
+    
+    # print the logon attempts vs time, for all terminal id's
+    df.pipe(hrc.plot_utc_sof,'id')
+    
+    id_list = df['id'].unique()
+    print(str(len(id_list)) +  " unique terminal id\'s in log:")
+    print(id_list)
 
-# print the logon attempts vs time, for all terminal id's
-df.pipe(hrc.plot_utc_sof,'id')
 
-id_list = df['id'].unique()
-print(str(len(id_list)) +  " unique id\'s in log:")
-print(id_list)
-
+if __name__ == "__main__":
+    main(sys.argv)
 
 
--- usecases/check_performance.py
+++ usecases/check_performance.py
@@ -10,8 +10,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import hrclogutils.carrier as carrier
 
+def main(argv):
+    if len(argv)==3:
+        df = carrier.load_carrier_log(argv[1],argv[2])
+    else:
+        df = carrier.load_carrier_log(argv[1], "../tests/sbc_carrier_tracking_monitor.headers")
 
 
-df = carrier.load_carrier_log(path="../tests/sbc_carrier_tracking_monitor.csv",header_path="../tests/sbc_carrier_tracking_monitor.headers")
-dfpivot = df.pipe(carrier.terminal_minmax,'curEsNo(dB)','modcod','spreadingFactor');
-print(dfpivot)
+    dfpivot = df.pipe(carrier.terminal_minmax,'curEsNo(dB)','modcod','spreadingFactor');
+    print(dfpivot)
+    
+
+if __name__ == "__main__":
+    main(sys.argv)
+    
