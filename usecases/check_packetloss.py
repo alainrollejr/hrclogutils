8dd500a23c7fac19345af8b61de783626b9c102e
alainrollejr <35453310+alainrollejr@users.noreply.github.com>
2018-01-18 13:11:38 +0100

    turning into a script that can be called with arguments

--- usecases/check_packetloss.py
+++ usecases/check_packetloss.py
@@ -2,7 +2,9 @@
 """
 Created on Sat Jan 13 23:53:29 2018
 
-@author: aro
+ python check_packetloss.py 15843 ../tests/sbc_carrier_tracking_monitor.csv
+ in Spyder IDU %run check_packetloss.py 15843 ../tests/sbc_carrier_tracking_monitor.csv
+ 
 """
 import os
 import sys
@@ -10,6 +12,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import hrclogutils.carrier as carrier
 
-idsubstring = '15843'
 
-carrier.packet_error_analysis(idsubstring,startDateTime="2018-01-15T11:23:59")
+def main(argv):
+    if len(argv)==3:
+        carrier.packet_error_analysis(idsubstring=argv[1],path=argv[2],header_path="../tests/sbc_carrier_tracking_monitor.headers")
+       
+    elif len(argv)==1:
+        idsubstring = '15843'
+        carrier.packet_error_analysis(idsubstring,startDateTime="2018-01-15T11:23:59", path="../tests/sbc_carrier_tracking_monitor.csv", header_path="../tests/sbc_carrier_tracking_monitor.headers")
+
+if __name__ == "__main__":
+    main(sys.argv)
