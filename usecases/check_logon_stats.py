819cb8ed9e1fbd06f95c6d1b866e5e8f28efc839
alainrollejr <35453310+alainrollejr@users.noreply.github.com>
2018-01-24 16:48:04 +0100

    proper argv processing

--- usecases/check_logon_stats.py
+++ usecases/check_logon_stats.py
@@ -12,18 +12,46 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import argparse
+
 import hrclogutils.basic as hrc
 import hrclogutils.rtce as rtce
 
 
 
 def main(argv):
-    if len(argv)==3:
-        df = rtce.load_rtce_log(argv[1],argv[2])
-    elif len(argv)==2:
-        df = rtce.load_rtce_log(argv[1], "../tests/sbc_rtce_monitor.headers")
-    else:
-        df = rtce.load_rtce_log("./sbc_rtce_monitor.csv", "../tests/sbc_rtce_monitor.headers")
+    
+    parser = argparse.ArgumentParser(description='get overview of logon attempts for terminals')
+    
+    parser.add_argument('-p','--path', help='path to sbc_rtce_monitor.csv', required=False)
+    parser.add_argument('-c','--columns', help='path to sbc_rtce_monitor.headers', required=False)
+    parser.add_argument('-b','--beginTime', help='begin time eg 2018-01-15T11:23:59', required=False)
+    parser.add_argument('-e','--endTime', help='end time eg 2018-01-15T12:23:59', required=False)
+    args = vars(parser.parse_args())
+   
+    
+   
+    path = args['path']
+    header_path = args['columns']
+    startDateTime = args['beginTime']
+    stopDateTime = args['endTime']
+    
+   
+    if path is None:
+        path="./sbc_rtce_monitor.csv"
+        
+    if header_path is None:
+        header_path="../tests/sbc_rtce_monitor.headers"
+        
+    if startDateTime is None:
+        startDateTime="1977-06-02T13:45:30"
+        
+    if stopDateTime is None:
+        stopDateTime="2200-06-15T13:45:30"
+    
+    
+        
+    df = rtce.load_rtce_log(path, header_path)
     
     
     # filter to display terminals only when they are logging on
