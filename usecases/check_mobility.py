1a5c69dfc0e69aad3564767c1d42c1435cb1b215
alainrollejr <35453310+alainrollejr@users.noreply.github.com>
2018-02-12 18:53:40 +0100

    adding median time between state change

--- usecases/check_mobility.py
+++ usecases/check_mobility.py
@@ -47,11 +47,24 @@ def main(argv):
         df.pipe(dmm.scatter_on_basemap, title = 'GPS data from terminals')
         df.pipe(dmm.beams_on_basemap, title = 'GPS data from terminals')
         
-        print(" ")
-        print("nr of state changes (operational/nonoperational) sorted per mac (high numbers are suspicious and point at RF issues and flapping)")
-        print("=================================================================================================================================")
-        dfChanges_freq = dfChanges['mac'].value_counts()
-        print(dfChanges_freq)
+               
+        dfChanges.pipe(hrc.plot_utc, 'operational')
+    
+        macList = dfChanges['mac'].unique()
+        columns = ['mac','nr_of_statechanges','median_time_between_changes']
+        dfChanges_stats = pd.DataFrame(columns=columns)
+        dfChanges_stats = dfChanges_stats.fillna(0) # with 0s rather than NaNs
+        
+        for mac in macList:
+            dfSubset = dfChanges[dfChanges['mac']==mac]
+            nr_element = len(dfSubset)
+            dfSubset['timediff'] = dfSubset['dateTimes'] - dfSubset['dateTimes'].shift(1)
+            timediff_median = dfSubset['timediff'].median()
+            row=pd.Series([mac, nr_element,timediff_median],columns)
+            dfChanges_stats = dfChanges_stats.append([row],ignore_index=True)
+        
+        dfChanges_stats = dfChanges_stats.sort_values(['nr_of_statechanges'], ascending=False)
+        print(dfChanges_stats)
             
         dfStats = dmm.stats_to_dataframe(path)
         #print(dfStats.head())
@@ -67,7 +80,21 @@ def main(argv):
         df.pipe(hrc.plot_utc,'lat','long')  
         
         dfChanges.pipe(hrc.plot_utc, 'operational')
+    
+        macList = dfChanges['mac'].unique()
+        columns = ['mac','nr_of_statechanges','median_time_between_changes']
+        dfChanges_stats = pd.DataFrame(columns=columns)
+        dfChanges_stats = dfChanges_stats.fillna(0) # with 0s rather than NaNs
         
+        for mac in macList:
+            dfSubset = dfChanges[dfChanges['mac']==mac]
+            nr_element = len(dfSubset)
+            dfSubset['timediff'] = dfSubset['dateTimes'] - dfSubset['dateTimes'].shift(1)
+            timediff_median = dfSubset['timediff'].median()
+            row=pd.Series([mac, nr_element,timediff_median],columns)
+            dfChanges_stats = dfChanges_stats.append([row],ignore_index=True)
+            
+        print(dfChanges_stats.head())
     
 if __name__ == "__main__":
     main(sys.argv)
