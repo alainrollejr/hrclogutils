77b1bb3a49b4072a623f97654441f8fc93c6306a
alainrollejr <35453310+alainrollejr@users.noreply.github.com>
2018-03-13 19:07:03 +0100

    utc times extracted

--- tests/test_flightradar.py
+++ tests/test_flightradar.py
@@ -9,22 +9,45 @@ Created on Tue Mar 13 11:54:16 2018
 
 """
 import json
+from datetime import datetime
 from pyflightdata import FlightData
 
 api=FlightData()
 
-max_nr_entries = 100
+max_nr_entries = 20
 r = api.get_history_by_tail_number('N812UA')[-max_nr_entries:]
 
 m = json.dumps(r)
 rjson = json.loads(m)
 
 #print(rjson)
+
 for item in rjson:
-    # first items are most recent
-    print('from :' + str(item["airport"]["origin"]["name"]))
-    print('to: ' + str(item["airport"]["destination"]["name"]))
-    print('status: ' + str(item["status"]["text"]))
-    print(item["time"]["real"])
-    print("\n")
+    # first items are most recent    
+    status = item["status"]["generic"]["status"]["text"]
+    
+    if status == "landed": # everything else is scheduled or estimated
+        print('from :' + str(item["airport"]["origin"]["name"]))
+        print('to: ' + str(item["airport"]["destination"]["name"]))
+        print('status: ' + str(status))
+        
+        try:
+            departure_utc = datetime.utcfromtimestamp(int(item["time"]["real"]["departure"]))
+            departure_known = True
+        except:
+            departure_known = False
+            
+        try:
+            arrival_utc = datetime.utcfromtimestamp(int(item["time"]["real"]["arrival"]))
+            arrival_known = True
+        except:
+            arrival_known = False
+        
+        if departure_known:
+            print("departure " + str(departure_utc))
+            
+        if arrival_known:
+            print("arrival " + str(arrival_utc))
+        
+        print("\n")
     
