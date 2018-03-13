0c2ba791193f5e1478addcbb84db94fedcd549a2
alainrollejr <35453310+alainrollejr@users.noreply.github.com>
2018-03-13 22:40:10 +0100

    backup

---
+++ usecases/flightradar_for_mac.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 13 21:28:37 2018
+
+@author: aro
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import argparse
+import hrclogutils.basic as hrc
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import json
+from datetime import datetime
+from pyflightdata import FlightData
+
+
+def get_flightradar_info_for_mac(mac,path_to_modem_list):
+    modem_df = pd.read_csv(path_to_modem_list)  
+    row = modem_df[modem_df['mac']==mac]
+    tail = row.iloc[0]['tail']
+    print('tail = ' + str(tail))
+    return tail
+
+def main(argv):
+    
+    pd.options.mode.chained_assignment = None  # default='warn'
+    
+    parser = argparse.ArgumentParser(description='script to check flight radar data for a terminal')
+    
+    parser.add_argument('-m','--mac', help='mac address of terminal, eg -m 00:0d:2e:00:02:81 (no quotes !)', required=True)
+    parser.add_argument('-p','--path', help='path to dmm.log.* file', required=False)
+    parser.add_argument('-l','--modemlist', help='path to modem_list.csv, which can be fetched via rest_api_all_modems.py', required=True)
+    
+    args = vars(parser.parse_args())
+   
+    mac = args['mac']
+    
+    path = args['path']
+    path_to_modem_list = args['modemlist']
+    
+    tail =get_flightradar_info_for_mac(mac,path_to_modem_list)
+
+    api=FlightData()
+    
+    max_nr_entries = 100
+    r = api.get_history_by_tail_number(tail)[-max_nr_entries:]
+    
+    m = json.dumps(r)
+    rjson = json.loads(m)
+    
+    #print(rjson)
+    
+    for item in rjson:
+        # first items are most recent    
+        status = item["status"]["generic"]["status"]["text"]
+        
+        if status == "landed": # everything else is scheduled or estimated
+            print('from :' + str(item["airport"]["origin"]["name"]))
+            print('to: ' + str(item["airport"]["destination"]["name"]))
+            print('status: ' + str(status))
+            
+            try:
+                departure_utc = datetime.utcfromtimestamp(int(item["time"]["real"]["departure"]))
+                departure_known = True
+            except:
+                departure_known = False
+                
+            try:
+                arrival_utc = datetime.utcfromtimestamp(int(item["time"]["real"]["arrival"]))
+                arrival_known = True
+            except:
+                arrival_known = False
+            
+            if departure_known:
+                print("departure " + str(departure_utc))
+                
+            if arrival_known:
+                print("arrival " + str(arrival_utc))
+            
+            print("\n")
+            
+    print(len(rjson))
+    
+       
+    
+if __name__ == "__main__":
+    main(sys.argv)
+        
+
